e9e8e1a209f67928cf11b912ea5c87314d0b1834
abcustodio1 <abcustodio1@up.edu.ph>
2019-04-24 05:11:51 +0800

    Added n-chancellor solver for blank board config

--- solver_with_debug_printing.c
+++ solver_with_debug_printing.c
@@ -7,15 +7,20 @@
 int isSafe(int nopts[N+2], int row, int col, int last) {
   int i;
 
+  printf("row: %d | col: %d | last: %d\n", row, col, last);
+
   // if filled before - safe but not allowed
   if (last == col) {
+    printf("ALREADY MARKED AS CHANCELLOR\n");
     return FALSE;
   }
 
   // i is the current row - nopts[i] is the current col
   for(i=1; i<N+1; i++) {
+    printf("i: %d | nopts[i]: %d\n", i, nopts[i]);
     // rook movement:
     if (nopts[i] != 0 && (row == i || col == nopts[i])) {
+      printf("ROOK MOVEMENT\n");
       return FALSE;                  
     }
     // horse movements:
@@ -26,6 +31,7 @@ int isSafe(int nopts[N+2], int row, int col, int last) {
       x 0
     */
     if (nopts[i] != 0 && (i+2 == row && nopts[i]-1 == col)) {
+      printf("KNIGHT MOVEMENT\n");
       return FALSE;
     }
 
@@ -34,6 +40,7 @@ int isSafe(int nopts[N+2], int row, int col, int last) {
       x 0 0
     */
     else if (nopts[i] != 0 && (i+1 == row && nopts[i]-2 == col)) {
+      printf("KNIGHT MOVEMENT\n");
       return FALSE;
     }
 
@@ -42,6 +49,7 @@ int isSafe(int nopts[N+2], int row, int col, int last) {
       0 0 x
     */
     else if (nopts[i] != 0 && (i+1 == row && nopts[i]+2 == col)) {
+      printf("KNIGHT MOVEMENT\n");
       return FALSE;
     }
 
@@ -51,6 +59,7 @@ int isSafe(int nopts[N+2], int row, int col, int last) {
       0 x
     */
     else if (nopts[i] != 0 && (i+2 == row && nopts[i]+1 == col)) {
+      printf("KNIGHT MOVEMENT\n");
       return FALSE;
     }
 
@@ -58,6 +67,7 @@ int isSafe(int nopts[N+2], int row, int col, int last) {
   
   // if all else fails and position is blank - safe
   if (nopts[row] == 0) {
+    printf("SAFE FOR CHANCELLOR\n");
     return TRUE;
   }
   // the position is safe
@@ -102,7 +112,6 @@ int main() {
         solutions++;
         printf("Solution found!\n");
         printBoard(option);
-        printf("\n");
       }
 
       // initial first chancellor
@@ -115,6 +124,7 @@ int main() {
           nopts[move] = last + 1;
           option[move][nopts[move]] = 1;
           last = -1;
+          printf("move: %d | nopts[move]: %d\n", move, nopts[move]);
         }
       }
 
@@ -123,22 +133,27 @@ int main() {
         if(last == -1) i = 1;
         else i = last;
         for (i; i<N+1; i++) {
+          printf("\n");
+          printBoard(option);
+          printf("\n");
           if (isSafe(nopts, move, i, last)) {
             option[move][i] = 1; // mark as chancellor
             nopts[move] = i; // store index of stack
             last = -1;
+            printf("\n");
+            printBoard(option);
+            printf("\n");
             break;
           }
         }
 
-        // BACK TRACK
         if (i == N+1) {
           nopts[move] = 0;
           move --;
           option[move][nopts[move]] = 0; // clear chancellor
           last = nopts[move];
-          // ANOTHER BACKTRACK IF THE PREV MOVE EXHAUSTED ALL POSSIBLE CANDIDATES
           if(last == N){
+            printf("Last: %d | i: %d | move: %d\n", last, i, move);
             nopts[move] = 0;
             move--;
             if(move == 0) nopts[move] = 0;
